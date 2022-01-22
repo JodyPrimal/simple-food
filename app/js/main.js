cab93d27b5507313e87943b6a60c110dd05fa235
JodyPrimal <vlaguvlagu@gmail.com>
2022-01-22 21:23:56 +0200

    add catalog

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,28 @@
 $(function(){
+
+
+$('.select__style').styler();
+
+$('.filter__price-input').ionRangeSlider({
+  type: "double",
+  prefix: "$",  
+  onStart: function (data){
+    $('.filter__price-from').text(data.from);
+    $('.filter__price-to').text(data.to);},
+  onChange: function (data) {
+    $('.filter__price-from').text(data.from);
+    $('.filter__price-to').text(data.to);
+        }
+});
+
+
+$('.feed__slider-inner').slick({
+  dots:true
+
+
+});
+
+
+
   var mixer = mixitup('.category__content');
-})
+});
